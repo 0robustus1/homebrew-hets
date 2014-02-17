80d33b0acfb2c32a20e41464f3c1c75a9bfba65a
Tim Reddehase <robustus@rightsrestricted.com>
2014-02-17 13:29:25 +0100

    correct the hets-dependencies formula.

--- hets_dependencies.rb
+++ hets_dependencies.rb
@@ -2,31 +2,15 @@ require 'formula'
 
 class HetsDependencies < Formula
 
+  url "https://gist.github.com/0robustus1/9049050/raw/57625927b7f155611eb0dfe353c96128dce45fe7/install_cabal_hets_deps.sh"
+  homepage 'https://gist.github.com/0robustus1/9049050'
+  sha1 'becccbe9553856203641bd18f054ecd0a3af7fcf'
+
   depends_on 'ghc'
   depends_on 'cabal-install'
 
   def install
-    programatica_url = 'http://www.dfki.de/sks/hets/src-distribution/programatica-1.0.0.4.tar.gz'
-    pre=`ghc --print-libdir | sed -e 's+/lib.*/.*++g'`
-    opts="--enable-documentation -p --global --prefix=$pre"
-
-
-    puts "Installing libraries..."
-    packages = %w{aterm
-      random utf8-string xml fgl HTTP haskeline
-      HaXml hexpat uni-uDrawGraph parsec1 wai-extra
-      warp tar}
-    silent_system("cabal install #{packages.join(' ')} #{opts}"
-
-    silent_system("cabal install #{programatica_url} --global --prefix=#{pre}")
-
-    silent_system("ghc-pkg hide parsec1")
-    silent_system("ghc-pkg hide programatica")
-
-  end
-
-  def silent_system(string)
-    system(string + " 2>&1 /dev/null")
+    system('bash install_cabal_hets_deps.sh')
   end
 
 end
