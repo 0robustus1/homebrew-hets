53edda6143a4ddfa83672264b806585204e5f0a0
Tim Reddehase <robustus@rightsrestricted.com>
2014-02-19 18:26:47 +0100

    add build-version of hets as default
    and rename the old binary version to hets-binary.

--- hets.rb
+++ hets.rb
@@ -3,90 +3,73 @@ require "formula"
 class Hets < Formula
   # Both the version and the sha1 need to be adjusted when a new
   # dmg-version of hets is released.
-  version "2014-01-06"
-  sha1 "8f7a8ee54c32e67933cb84f5c66fd08da98dd2a1"
-
-  # General settings: should not change usually
-  @@dmg_file = "Hets-#{version}.dmg"
-  @@nightly_url = "http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/intel-mac/daily/hets.bz2"
+  revision = 18505
   homepage "http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/index_e.htm"
-  url "http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/intel-mac/dmgs/#{@@dmg_file}"
-
-  # Options
-  option 'with-nightly', 'uses nightly version of the binary, also includes hets-update'
-  option 'with-updater', 'installs the updater script but does not run it'
+  head "https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk/", :using => :svn
+  url "https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk/", :using => :svn, :revision => revision
+  version "0.99-#{revision}"
 
   depends_on :x11
+  depends_on 'hets_dependencies'
+  depends_on 'hets-lib'
+  depends_on 'pellet'
+  depends_on 'uDrawGraph'
+
+  conflicts_with 'hets-binary'
+
+  option 'with-wrapper', 'install the wrapper script to actually run hets.'
 
   def install
-    dmg_content = '/Volumes/Hets'
-    app_dir = prefix.join('Hets.app')
-    hets_dir = app_dir.join('Contents/Resources')
-    binary = hets_dir.join('hets')
+    # ENV['PATH'] = "~/.cabal/bin:/usr/local/opt/ghc/bin:/usr/local/opt/cabal-install/bin:/usr/local/opt/uDrawGraph/bin:#{ENV['PATH']}"
 
-    system("hdiutil attach -quiet #{@@dmg_file}")
+    puts 'Compiling hets...'
+    system('make -j 1')
+    system('strip hets')
 
-    system("cp -r #{dmg_content}/Hets.app #{app_dir}")
+    puts 'Compiling owl-tools...'
+    system('make initialize_java')
 
-    # install hets in bin as script which sets according
-    # environment variables
-    # (taken and adjusted from script file in hets root dir)
-    bin.mkdir
-    bin.join("hets").open('w') do |f|
-      f.write <<-BASH
-#!/bin/bash
-export PATH=#{hets_dir}/uDrawGraph-3.1/bin:$PATH
-export HETS_LIB=#{hets_dir}/Hets-lib
-export HETS_OWL_TOOLS=#{hets_dir}/hets-owl-tools
-export HETS_APROVE=$HETS_OWL_TOOLS/AProVE.jar
-export HETS_ONTODMU=$HETS_OWL_TOOLS/OntoDMU.jar
-export PELLET_PATH=#{hets_dir}/pellet
-"#{hets_dir}/hets" "$@"
-      BASH
-    end
+    puts 'Putting everything together'
+    local_bin = prefix.join('bin')
+    local_lib = prefix.join('lib')
 
-    system("hdiutil detach -quiet #{dmg_content}")
+    local_bin.mkpath
+    local_lib.mkpath
 
-    # automatically install latest nightly build
-    if build.with? 'nightly'
-      system("curl -O #{@@nightly_url}")
-      system("bunzip2 hets.bz2")
-      system("mv hets #{binary}")
-      system("chmod +x #{binary}")
-    end
+    local_bin.install('hets')
 
-    # hets updater script to manually fetch latest nightly build
-    if build.with?('nightly') || build.with?('updater')
-      bin.join("hets-update").open('w') do |f|
+    owl_tools = local_lib.join('hets-owl-tools')
+
+    owl_tools.mkpath
+
+    owl_tools.install('OWL2/OWL2Parser.jar')
+    owl_tools.install('OWL2/OWLLocality.jar')
+    owl_tools.install('DMU/OntoDMU.jar')
+    owl_tools.install('CASL/Termination/AProVE.jar')
+    owl_tools.join('lib').mkpath
+    owl_tools.join('lib').install('OWL2/lib/owl2api-bin.jar')
+
+    if build.with? 'wrapper'
+      puts 'Install the wrapper script'
+      FileUtils.mv bin.join('hets').to_s, bin.join('hets-bin').to_s
+      # install hets in bin as script which sets according
+      # environment variables
+      # (taken and adjusted from script file in hets root dir)
+      bin.join("hets").open('w') do |f|
         f.write <<-BASH
 #!/bin/bash
-echo 'Downloading current nightly build of hets binary...'
-curl -q -o /tmp/hets.bz2 #{@@nightly_url}
-bunzip2 /tmp/hets.bz2
-echo -n 'Installing file...'
-mv /tmp/hets #{binary}
-chmod +x #{binary}
-echo 'done'
-        BASH
-      end
+export HETS_LIB=/usr/local/opt/hets-lib
+export HETS_OWL_TOOLS=/usr/local/opt/hets/lib/hets-owl-tools
+export HETS_APROVE=$HETS_OWL_TOOLS/AProVE.jar
+export HETS_ONTODMU=$HETS_OWL_TOOLS/OntoDMU.jar
+export PELLET_PATH=/usr/local/opt/pellet
+"/usr/local/opt/hets/bin/hets-bin" "$@"
+      BASH
     end
-
+    end
   end
 
   def caveats
-    the_caveats = ''
-    if build.with? 'nightly'
-      the_caveats += <<-EOS.undent
-        We've updated your hets binary to the latest nightly version.
-      EOS
-    end
-    if build.with?('nightly') || build.with?('updater')
-      the_caveats += <<-EOS.undent
-        We've also installed the `hets-update` script into your PATH
-        which will fetch you the latest nightly hets binary.
-      EOS
-    end
-    return the_caveats
   end
 
 end
