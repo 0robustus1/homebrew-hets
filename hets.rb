41d6711626459469e7ea5dd9bffd8ed0c5a3828e
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-03 12:01:05 +0100

    add ant as hets dependency.

--- hets.rb
+++ hets.rb
@@ -14,6 +14,7 @@ class Hets < Formula
   depends_on 'hets-dependencies'
   depends_on 'hets-lib'
   depends_on 'pellet'
+  depends_on 'ant'
   depends_on 'uDrawGraph'
 
   conflicts_with 'hets-binary'
