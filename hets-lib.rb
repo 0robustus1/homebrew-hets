c66de6f9e78f21d2bb578c8d6e8036f4290a16ac
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-03 12:00:40 +0100

    convert hets-lib to a svn-repository.

--- hets-lib.rb
+++ hets-lib.rb
@@ -3,13 +3,11 @@ require "formula"
 class HetsLib < Formula
   # Both the version and the sha1 need to be adjusted when a new
   # dmg-version of hets is released.
-  # version "2014-01-06"
-  # sha1 "8f7a8ee54c32e67933cb84f5c66fd08da98dd2a1"
 
+  @@revision = 1830
   homepage 'http://www.informatik.uni-bremen.de/cofi/Libraries/'
-  url "http://www.informatik.uni-bremen.de/cofi/Libraries/lib.tgz"
-  version 'unknown'
-  sha1 '25156a085a0f299f93ca49c6232ca5836e393e5f'
+  url "https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk/", :using => :svn, :revision => @@revision
+  version "unknown-#{@@revision}"
 
   def install
     prefix.install Dir['*']
