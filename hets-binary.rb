d7bd93b67cb29a1f554248089d9d620c1092b89b
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-18 14:47:58 +0100

    bump version of hets-binary to 2014-03-04.

--- hets-binary.rb
+++ hets-binary.rb
@@ -3,7 +3,7 @@ require "formula"
 class HetsBinary < Formula
   # Both the version and the sha1 need to be adjusted when a new
   # dmg-version of hets is released.
-  version "2014-01-06"
+  version "2014-03-04"
   sha1 "8f7a8ee54c32e67933cb84f5c66fd08da98dd2a1"
 
   # General settings: should not change usually
