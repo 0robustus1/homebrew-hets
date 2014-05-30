8b01f5059a160cf577dc23e2e4f4d1ca297bfc0f
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-05-30 12:46:46 +0200

    use existing url to programatica.

--- hets-dependencies.rb
+++ hets-dependencies.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class HetsDependencies < Formula
 
-  url "https://gist.githubusercontent.com/0robustus1/9049050/raw/62b6f5a86b975e7c2db671c8c631643504d5f50e/install_cabal_hets_deps.sh"
+  url "https://gist.githubusercontent.com/0robustus1/9049050/raw/a9c8e0dcbefef624090b6d3bce279fd73c579598/install_cabal_hets_deps.sh"
   homepage 'https://gist.github.com/0robustus1/9049050'
-  sha1 'f44ba593fd804baf9b18a94a8499e57228258345'
+  sha1 'cfa0ebdb51e6654ca3cea926b8e3106cda178de1'
 
   depends_on 'ghc'
   depends_on 'cabal-install'
