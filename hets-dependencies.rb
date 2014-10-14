42c99ddda6aa6f93c0fd09fc78510ad02a19950f
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-10-14 15:55:16 +0200

    bump hets-dependencies version.

--- hets-dependencies.rb
+++ hets-dependencies.rb
@@ -7,7 +7,7 @@ class HetsDependencies < Formula
   url "https://gist.githubusercontent.com/0robustus1/9049050/raw/a9c8e0dcbefef624090b6d3bce279fd73c579598/install_cabal_hets_deps.sh"
   homepage 'https://gist.github.com/0robustus1/9049050'
   sha1 'cfa0ebdb51e6654ca3cea926b8e3106cda178de1'
-  version '0.1.2'
+  version '0.1.3'
 
   depends_on 'ghc'
   depends_on 'cabal-install'
