9c0cc4b184c8b6e5e57f0e9dbd2024d0aa107dbb
Tim Reddehase <robustus@rightsrestricted.com>
2015-06-01 11:26:20 +0200

    update hets-lib and switch to git-repository.

--- hets-lib.rb
+++ hets-lib.rb
@@ -4,9 +4,9 @@ class HetsLib < Formula
   # Both the version and the sha1 need to be adjusted when a new
   # dmg-version of hets is released.
 
-  @@revision = 1830
+  @@revision = 'ecafeecc94554392d70022a2d9e9fce1448298c8'
   homepage 'http://www.informatik.uni-bremen.de/cofi/Libraries/'
-  url "https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk/", :using => :svn, :revision => @@revision
+  url "https://github.com/spechub/Hets-lib.git", :using => :git, :revision => @@revision
   version "unknown-#{@@revision}"
 
   def install
